8cd10eb70d27fa99d752c37f68cfbfade4e29a9a
echan1912 <51402884+echan1912@users.noreply.github.com>
2021-05-05 23:38:31 +0800

    Create blocktrans.sol

---
+++ blocktrans.sol
@@ -0,0 +1,15 @@
+//specify solidity version
+pragma solidity ^0.4.24;
+ 
+ contract TransEntry{
+     string blocktrans;
+     //create a function that writes a mood to the smart contract
+    function setMood(string _blocktrans) public{
+     tran = _tran;
+ }
+ 
+ //create a function the reads the mood from the smart contract
+     function gettran() public view returns(string){
+     return mood;
+ }
+ }
